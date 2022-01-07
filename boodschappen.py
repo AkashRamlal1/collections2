b5701a8b1ea1fa0a542a9d698b769293b91b5420
Akash Ramlal <akashramlal@MBP-van-Akash.home>
2022-01-07 22:32:23 +0100

    updated versie

--- boodschappen.py
+++ boodschappen.py
@@ -1,23 +1,30 @@
 boodschappenlijst = []
 
+
 kopen = True
 
 while kopen == True:
     
     toevoegen = input('wat wil je kopen?')
     boodschappenlijst.append(toevoegen)
+
+    aantal = int(input('hoeveel wil je ervan'))
+    
     print("op je lijstje staat")
     print("-----")
     for item in boodschappenlijst:
-        print("-  " + item)
+        print("-  " + int(aantal), item)
     print("-----")
 
     antwoord = input("wil je meer toevoegen?  (J/N)")
     if antwoord == "N" or antwoord == "n":
         kopen = False
+
+    if aantal in boodschappenlijst:
+        boodschappenlijst[toevoegen] += aantal
     
     print("dit wil je kopen:  ")
     print("-----")
     for item in boodschappenlijst:
-        print("-  " + item)
+        print("-  " + int(aantal), item)
     print("-----")
